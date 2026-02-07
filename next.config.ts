c7746aef29b2912f54edf719b054239ff8b58c98
윤동제 <dj.yoon@brycenkorea.co.kr>
2026-02-07 20:27:45 +0900

    aa

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    output: 'export',        // <- 정적 export 옵션 추가
+    // 기존 옵션들
 };
 
 export default nextConfig;
